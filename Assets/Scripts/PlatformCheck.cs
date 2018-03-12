575435d52b8cde324d0b47b86814733d2f636e88
DAckDesign <anddack@hotmail.it>
2018-03-12 16:06:27 +0000

    Added platform check
    Now floating platforms only collide if the player's groundCheck tranfsorm is above the collision box of the platform itself.

---
+++ Assets/Scripts/PlatformCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCheck : MonoBehaviour {
+	PlayerKidController PlayerController;
+	// Use this for initialization
+	void Start () {
+		PlayerController = FindObjectOfType<PlayerKidController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerController.groundCheck.transform.position.y > gameObject.GetComponent<BoxCollider2D>().transform.position.y)
+		{
+			gameObject.GetComponent<BoxCollider2D>().enabled = true;
+		}
+		else gameObject.GetComponent<BoxCollider2D>().enabled = false;
+	}
+}
