6da7492077b2e9827e619004cca54b6a58c018d1
DAckDesign <anddack@hotmail.it>
2018-03-22 13:57:57 +0000

    StatueTrigger

---
+++ Assets/Scripts/StatueCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueCheck : MonoBehaviour {
+	PlayerKidController PlayerContr;
+	public int RequiredSouls;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (Input.GetButtonDown("Interact") && PlayerContr.SoulsCollected >= RequiredSouls)
+			{
+				GetComponent<Animator>().SetBool ("CollectionCompleted", true);
+			}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			PlayerContr = collision.GetComponent<PlayerKidController>();
+		}
+	}
+}
