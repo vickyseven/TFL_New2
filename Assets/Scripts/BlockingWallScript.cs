ed0a6134dd6ec9ef321bd95b1387b667c36f1e7d
DAckDesign <anddack@hotmail.it>
2018-03-23 15:31:41 +0000

    Barriers and souls
    Added barrier that either disappears or moves out of the way if a target object is destroyed.
    Souls now fade in, and float up and down.

---
+++ Assets/Scripts/BlockingWallScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockingWallScript : MonoBehaviour {
+	public GameObject ObjDependancy;
+	Animation Anim;
+	public bool TDestructOrFAnimate;
+	bool Animating;
+
+	// Use this for initialization
+	void Start () {
+		Anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!ObjDependancy) Remove();
+		if (Animating) gameObject.transform.position = Vector3.Max(gameObject.transform.position - new Vector3 (0f,0.1f,0f), new Vector3 (gameObject.transform.position.x, -23f, gameObject.transform.position.z));
+	}
+
+	void Remove()
+	{
+		if (TDestructOrFAnimate) Destroy(gameObject);
+		else Animating = true;
+	}
+}
--- Assets/Scripts/SoulCollect.cs
+++ Assets/Scripts/SoulCollect.cs
@@ -6,34 +6,48 @@ using UnityEngine;
 public class SoulCollect : MonoBehaviour
 {
 
-    public float soulAmount;
-    public AudioSource audioSoul;
+	public float soulAmount;
+	public AudioSource audioSoul;
+	bool IsAppearing;
+	Color SpriteColor;
 
+	// Use this for initialization
+	void Start()
+	{
+		SpriteColor = GetComponent<SpriteRenderer>().color;
+		IsAppearing = true;
+		SpriteColor = new Color(SpriteColor.r, SpriteColor.g, SpriteColor.b, 0f);
+		GetComponent<SpriteRenderer>().color = SpriteColor;
+	}
 
-    // Use this for initialization
-    void Start()
-    {
+	// Update is called once per frame
+	void Update()
+	{
+		if (IsAppearing && SpriteColor.a < 1)
+		{
+			SpriteColor = new Color(SpriteColor.r, SpriteColor.g, SpriteColor.b, SpriteColor.a + 0.05f);
+			GetComponent<SpriteRenderer>().color = SpriteColor;
+		}
+		else if (SpriteColor.a >= 1) IsAppearing = false;
+		Float();
+	}
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
 			other.GetComponent<PlayerKidController>().SoulsCollected++;
-            
-            Destroy(gameObject);
-        }
-    }
+			Destroy(gameObject);
+		}
+	}
 
-    private void OnDestroy()
-    {
+	private void OnDestroy()
+	{
 		AudioSource.PlayClipAtPoint(audioSoul.clip,gameObject.transform.position);
-    }
+	}
+
+	void Float()
+	{
+		gameObject.transform.position = gameObject.transform.position + new Vector3(0f, (Mathf.Sin(Time.time) / 100), 0f);
+	}
 }
