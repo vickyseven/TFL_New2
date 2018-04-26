39530e557128e0bf23c30d55e61e44963d6ce78e
DAckDesign <anddack@hotmail.it>
2018-04-26 12:07:27 +0100

    added itween, first attempt at camera changing script

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour {
+	public Camera NewCamera;
+	Camera OldCamera;
+
+	// Use this for initialization
+	void Start () {
+		OldCamera = FindObjectOfType<PlayerKidController>().GetComponentInChildren<Camera>();
+		NewCamera.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		OldCamera.enabled = false;
+		NewCamera.enabled = true;
+	}
+}
